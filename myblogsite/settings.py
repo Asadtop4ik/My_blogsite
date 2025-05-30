1ccaa32aff18199c380a5c71a5a82f668fb544cc
Asadtop4ik <asadbek.backend@gmail.com>
2025-05-30 11:27:46 +0500

    Add

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
-            'sslmode': 'disable',
+            'sslmode': 'prefer',
         },
     }
 }
